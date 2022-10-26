68be44c16ebb9bd4353e570c61700c68492c1777
David Trelles <davidstrelles@gmail.com>
2022-10-25 21:58:56 -0500

    did first three problems

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -10,6 +10,11 @@ console.log('***** Object Practice *****')
 */
 const me = {
   // TODO - add properties here
+  firstName: 'David',
+  lastName: 'Trelles',
+  hasSiblings: true,
+  shoeCount: 6,
+  favThreeFoods: ['pizza', 'tacos', 'pasta'],
 };
 console.log('A little about me:', me);
 
@@ -19,8 +24,8 @@ console.log('A little about me:', me);
     created above to set its value. 
   - Console.log fullName
 */
-
-
+let fullName = me.firstName + " " + me.lastName
+console.log(fullName)
 
 /* 3. Nested arrays
   - Console.log your first favorite food
